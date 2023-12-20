b70a3c41e76f9179a1817c8e048192e7536d1d4f
n1tsua5 <nitsua5@proton.me>
2023-12-20 15:24:59 -0500

    filters working

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -23,6 +23,16 @@ function paintToCanvas() {
 
     setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+        // take pixels out
+        let pixels = ctx.getImageData(0, 0, width, height);
+        // utilize them
+        // pixels = redEffect(pixels);
+
+        // pixels = rgbSplit(pixels);
+        // ctx.globalAlpha = 0.8;
+        pixels = greenScreen(pixels);
+        // put them back in place
+        ctx.putImageData(pixels, 0, 0)
     }, 16);
 }
 
@@ -36,9 +46,54 @@ function takePhoto() {
     const link = document.createElement('a');
     link.href = data;
     link.setAttribute('download', 'handsome');
-    link.textContent = 'Download Image';
+    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
     strip.insertBefore(link, strip.firstChild);
 }
+
+function redEffect(pixels) {
+    for(let i = 0; i < pixels.data.length; i+=4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 200; // red
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; // green
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // blue
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for(let i = 0; i < pixels.data.length; i+=4) {
+        pixels.data[i - 150] = pixels.data[i + 0] // red
+        pixels.data[i + 500] = pixels.data[i + 1] // green
+        pixels.data[i - 550] = pixels.data[i + 2] // blue
+    }
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.Value;
+    });
+
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+
+        if (red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red <= levels.rmax
+            && green <= levels.gmax
+            && blue <= levels.bmax) {
+            // take it out!
+            pixels.data[i + 3] = 0;
+        }
+    }
+    return pixels;
+}
+
 
 getVideo();
 
